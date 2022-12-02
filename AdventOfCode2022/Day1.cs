99844fdb8731fbd503a525bbf5db677983255ab6
Nicolò Sponziello <nicolosponziello@gmail.com>
2022-12-02 23:58:56 +0100

    Initial commit

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class Day1
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            var allCalories = new List<int>();
+            var counting = 0;
+            foreach(string line in lines)
+            {
+                if(string.IsNullOrWhiteSpace(line))
+                {
+                    allCalories.Add(counting);
+                    counting = 0;
+                }
+                else
+                {
+                    counting += int.Parse(line);
+                }
+            }
+            allCalories.Sort();
+            allCalories.Reverse();
+            Console.WriteLine(allCalories[0] + allCalories[1] + allCalories[2]);
+        }
+    }
+}
